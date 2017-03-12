230f92115961310f7dacde799b1e5eb6b13775ff
Dustin Falgout <dustin@falgout.us>
2017-03-12 03:10:32 -0500

    more work on containers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Give Redis some time to come up
+# Set nginx config
+cp "/etc/nginx/nginx.${NODE_ENV}.conf" /data/nginx.conf
+
+# Give redis some time to come up
 sleep 10
 
 ./nodebb upgrade && ./nodebb dev
 
-exit 1
+exit $?
